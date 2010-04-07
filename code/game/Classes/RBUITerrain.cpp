87251db99fe9610086c6d426dec8def358781a87
rose <rose@GKQ>
2010-04-06 23:50:42 -0800

    adding ui terrain
    [git-p4: depot-paths = "//depot/Golf/": change = 1040]

---
+++ code/game/Classes/RBUITerrain.cpp
@@ -0,0 +1,122 @@
+/*
+ *  RBUITerrain.cpp
+ *  golf
+ *
+ *  Created by Robert Rose on 4/30/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "RBUITerrain.h"
+
+#include "RudeGL.h"
+#include "RudeFont.h"
+#include "RudeSound.h"
+#include "RudeTextureManager.h"
+
+RBUITerrain::RBUITerrain()
+: m_mode(kTerrainModeNone)
+{
+	m_touch[0] = 0;
+	m_touch[1] = 0;
+
+	m_tvc.SetRect(RudeRect(0,0,1024,768));
+}
+
+RBUITerrain::~RBUITerrain()
+{
+}
+
+void RBUITerrain::StartDisplay()
+{
+	m_done = false;
+	m_mode = kTerrainModeNone;
+	m_touch[0] = 0;
+	m_touch[1] = 0;
+}
+
+
+void RBUITerrain::NextFrame(float delta)
+{
+	if(m_done)
+		return;
+	
+}
+
+void RBUITerrain::Render(float width, float height)
+{
+	if(m_done)
+		return;
+	
+	m_tvc.Render();
+}
+
+void RBUITerrain::TouchDown(RudeTouch *rbt)
+{
+	if(m_done)
+		return;
+
+	switch(m_mode)
+	{
+	case kTerrainModeNone:
+		m_mode = kTerrainModeTranslate;
+		m_touch[0] = rbt;
+		m_pos[0] = rbt->m_location;
+		break;
+	case kTerrainModeTranslate:
+		m_touch[1] = rbt;
+		m_mode = kTerrainModeScale;
+		break;
+	case kTerrainModeScale:
+
+		break;
+	}
+	
+}
+
+void RBUITerrain::TouchMove(RudeTouch *rbt)
+{
+	if(rbt != m_touch[0] && rbt != m_touch[1])
+		return;
+
+	switch(m_mode)
+	{
+	case kTerrainModeNone:
+		break;
+	case kTerrainModeTranslate:
+		{
+			RudeScreenVertex diff = m_pos[0] - rbt->m_location;
+			m_tvc.Translate(diff.m_x, diff.m_y);
+
+			m_pos[0] = rbt->m_location;
+		}
+		break;
+	case kTerrainModeScale:
+		{
+			RudeScreenVertex pdiff = m_touch[0]->m_location - m_touch[1]->m_location;
+			
+		}
+		break;
+	}
+}
+
+void RBUITerrain::TouchUp(RudeTouch *rbt)
+{
+	switch(m_mode)
+	{
+	case kTerrainModeNone:
+		break;
+	case kTerrainModeTranslate:
+		m_mode = kTerrainModeNone;
+
+		if(rbt == m_touch[0])
+			m_touch[0] = 0;
+		break;
+	case kTerrainModeScale:
+		m_mode = kTerrainModeTranslate;
+
+		if(rbt == m_touch[1])
+			m_touch[1] = 0;
+		break;
+	}
+}
---
+++ code/game/Classes/RBUITerrain.h
@@ -0,0 +1,63 @@
+/*
+ *  RBUITerrain.h
+ *  golf
+ *
+ *  Created by Robert Rose on 4/30/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef __H_RBUITerrain
+#define __H_RBUITerrain
+
+#include "Rude.h"
+#include "RBGameBase.h"
+#include "RBTerrain.h"
+#include "RBTerrainViewControl.h"
+
+typedef enum {
+	kTerrainModeNone,
+	kTerrainModeTranslate,
+	kTerrainModeScale,
+} eTerrainMode;
+
+
+class RBUITerrain : public RBGameBase {
+public:
+	
+	RBUITerrain();
+	~RBUITerrain();
+	
+	void NextFrame(float delta);
+	void Render(float width, float height);
+
+	void StartDisplay();
+
+	void SetPositions(const btVector3 &ball, const btVector3 &hole)
+	{
+		m_tvc.SetPositions(ball, hole);
+	}
+	
+	void TouchDown(RudeTouch *rbt);
+	void TouchMove(RudeTouch *rbt);
+	void TouchUp(RudeTouch *rbt);
+
+
+	void SetTerrain(RBTerrain *terrain)
+	{
+		m_tvc.SetTerrain(terrain);
+	}
+
+private:
+
+	RBTerrainViewControl m_tvc;
+
+	RudeTouch *m_touch[2];
+	RudeScreenVertex m_pos[2];
+	
+	eTerrainMode m_mode;
+
+
+};
+
+#endif
