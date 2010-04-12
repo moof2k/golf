f2f30360048cc6b0e6703973966f46ecf263bd74
rose <rose@GKQ>
2010-04-12 00:21:16 -0800

    second pass on slice control adds a highlight line
    [git-p4: depot-paths = "//depot/Golf/": change = 1074]

--- code/game/Classes/RBTerrainSliceControl.cpp
+++ code/game/Classes/RBTerrainSliceControl.cpp
@@ -41,7 +41,11 @@ void RBTerrainSliceControl::Render()
 	float maxheight = m_guide->GetMaxGuidePoint();
 	float minheight = m_guide->GetMinGuidePoint();
 
-	float scale = (m_rect.m_bottom - m_rect.m_top) / (maxheight - minheight);
+	float heightdiff = (maxheight - minheight);
+	if(heightdiff < 1.0f)
+		heightdiff = 1.0f;
+
+	float scale = (m_rect.m_bottom - m_rect.m_top - 6) / heightdiff;
 	float center = (m_rect.m_bottom - m_rect.m_top) / 2.0f;
 
 	float segmentWidth = (m_rect.m_right - m_rect.m_left) / float(numGuidePoints-1);
@@ -56,35 +60,54 @@ void RBTerrainSliceControl::Render()
 
 		//const float kPointSize = 0.2;
 
+		float left = m_rect.m_right - i * segmentWidth;
+		float right = m_rect.m_right - (i-1) * segmentWidth;
+		float p0 = m_rect.m_bottom - h0 * scale;
+		float p1 = m_rect.m_bottom - h1 * scale;
+
 		GLfloat point[] = {
-			m_rect.m_right - (i-1) * segmentWidth, m_rect.m_bottom - h0 * scale,
-			m_rect.m_right - i * segmentWidth, m_rect.m_bottom - h1 * scale,
+			left, p1,
+			right, p0,
+			right, m_rect.m_bottom,
+			left, m_rect.m_bottom
 		};
 
-		float a = 0.7f;
+		const float kBgAlpha = 0.3f;
 
-
-
-		GLfloat colors[] = {
-			c0.m_r, c0.m_g, c0.m_b, a,
-			c1.m_r, c1.m_g, c1.m_b, a,
+		GLfloat bgcolors[] = {
+			c1.m_r, c1.m_g, c1.m_b, kBgAlpha,
+			c0.m_r, c0.m_g, c0.m_b, kBgAlpha,
+			c0.m_r, c0.m_g, c0.m_b, kBgAlpha,
+			c1.m_r, c1.m_g, c1.m_b, kBgAlpha,	
 		};
 
 
 		glVertexPointer(2, GL_FLOAT, 0, point);
-		glColorPointer(4, GL_FLOAT, 0, colors);
-		glDrawArrays(GL_LINES, 0, 4);
+		glColorPointer(4, GL_FLOAT, 0, bgcolors);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+		const float kLineAlpha = 0.7f;
+
+		GLfloat linecolors[] = {
+			c1.m_r, c1.m_g, c1.m_b, kLineAlpha,
+			c0.m_r, c0.m_g, c0.m_b, kLineAlpha,
+		};
+
+		glColorPointer(4, GL_FLOAT, 0, linecolors);
+		glDrawArrays(GL_LINES, 0, 2);
+	
 
 		if(i % 10 == 0)
 		{
+
 			GLfloat point[] = {
 				m_rect.m_right - i * segmentWidth, m_rect.m_bottom,
 				m_rect.m_right - i * segmentWidth, m_rect.m_bottom + 3,
 			};
 
 			glVertexPointer(2, GL_FLOAT, 0, point);
-			glColorPointer(4, GL_FLOAT, 0, colors);
-			glDrawArrays(GL_LINES, 0, 4);
+			glColorPointer(4, GL_FLOAT, 0, linecolors);
+			glDrawArrays(GL_LINES, 0, 2);
 		}
 	}
 }
