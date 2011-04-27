a5e4061c2c83b97c13dbda76280e1a73103f39fd
rose <rose@GKQ>
2011-04-26 21:08:29 -0800

    first pass on software skinned mesh renderer
    [git-p4: depot-paths = "//depot/Golf/": change = 1123]

--- code/engine/RudeSkinnedMesh.cpp
+++ code/engine/RudeSkinnedMesh.cpp
@@ -16,7 +16,6 @@
 #include "RudeTextureManager.h"
 #include "RudeTweaker.h"
 
-
 bool gDebugAnim = false;
 RUDE_TWEAK(DebugAnim, kBool, gDebugAnim);
 
@@ -64,6 +63,8 @@ int RudeSkinnedMesh::Load(const char *name)
 		RUDE_ASSERT(mesh->sBoneWeight.eType == EPODDataFloat, "Bone weight must be float");
 		RUDE_ASSERT(mesh->sVertex.eType == EPODDataFloat, "Mesh verts should be float");
 	}
+
+	//GLhandleARB g_vertexShader = glCreateShaderObjectARB( GL_VERTEX_SHADER_ARB );
 	
 	return 0;
 	
@@ -238,5 +239,142 @@ void RudeSkinnedMesh::Render()
 	
 	RUDE_PERF_STOP(kPerfRudeSkinMeshRender);
 	
+#else
+
+	
+	glMatrixMode(GL_MODELVIEW);
+	PVRTMATRIX viewmat;
+	glGetFloatv(GL_MODELVIEW_MATRIX, viewmat.f);
+	
+	RGL.Enable(kBackfaceCull, true);
+	
+	glCullFace(GL_FRONT);
+	glFrontFace(GL_CW);
+	
+	RGL.EnableClient(kVertexArray, true);
+	RGL.EnableClient(kTextureCoordArray, true);
+	
+	for(unsigned int i = 0; i < m_model.nNumNode; i++)
+	{
+		SPODNode *node = &m_model.pNode[i];
+		
+		if(!node->pszName)
+			continue;
+		if(node->pszName[0] != 'M')
+			continue;
+		
+		SPODMaterial *material = &m_model.pMaterial[node->nIdxMaterial];
+		SPODMesh *mesh = &m_model.pMesh[node->nIdx];
+		
+		int textureid = material->nIdxTexDiffuse;
+		if(textureid >= 0)
+			RudeTextureManager::GetInstance()->SetTexture(m_textures[textureid]);
+		
+		unsigned short *indices	= (unsigned short*) mesh->sFaces.pData;
+		
+		glVertexPointer(3, GL_FLOAT, mesh->sVertex.nStride, mesh->pInterleaved + (long)mesh->sVertex.pData);
+		
+		glTexCoordPointer(2, GL_FLOAT, mesh->psUVW->nStride, mesh->pInterleaved + (long)mesh->psUVW->pData);
+		
+		if((mesh->sVtxColours.n > 0) && (mesh->sVtxColours.eType == EPODDataRGBA))
+		{
+			RGL.EnableClient(kColorArray, true);
+			glColorPointer(4, GL_UNSIGNED_BYTE, mesh->sVtxColours.nStride, mesh->pInterleaved + (long)mesh->sVtxColours.pData);
+		}
+		else
+		{
+			RGL.EnableClient(kColorArray, false);
+			glColor4f(1.0, 1.0, 1.0, 1.0);
+		}
+		
+		int totalbatchcnt = 0;
+		
+		for(int b = 0; b < mesh->sBoneBatches.nBatchCnt; b++)
+		{
+			int batchcnt = mesh->sBoneBatches.pnBatchBoneCnt[b];
+
+			btTransform boneworldbt[9];
+
+			if(m_animate)
+			{
+				for(int j = 0; j < batchcnt; ++j)
+				{
+					// Generates the world matrix for the given bone in this batch.
+					
+					PVRTMATRIX	boneworld;
+
+					int i32NodeID = mesh->sBoneBatches.pnBatches[j + totalbatchcnt];
+					m_model.GetBoneWorldMatrix(boneworld, *node, m_model.pNode[i32NodeID]);
+
+					boneworldbt[j].setFromOpenGLMatrix(boneworld.f);
+				}
+			}
+			
+			totalbatchcnt += batchcnt;
+			
+			int offset = mesh->sBoneBatches.pnBatchOffset[b] * 3;
+			int end = mesh->sBoneBatches.pnBatchOffset[b+1] * 3;
+			
+			if(b == (mesh->sBoneBatches.nBatchCnt - 1))
+				end = mesh->nNumFaces*3;
+			
+			int numidx = (end - offset);
+
+			for(int v = offset; v < end; v += 3)
+			{
+				unsigned short *idx = &indices[v];
+
+				float verts[3][3] = { 0.0 };
+				float uvs[3][2] = { 0.0 };
+				float weights[3][9] = { 0.0 };
+				unsigned char bones[3][9] = { 0 };
+
+				for(int a = 0; a < 3; a++)
+				{
+					float *meshverts = (float*) (mesh->pInterleaved + (long)mesh->sVertex.pData + mesh->sVertex.nStride * indices[v+a]);
+					float *meshuvs = (float*) (mesh->pInterleaved + (long)mesh->psUVW->pData + mesh->psUVW->nStride * indices[v+a]);
+					float *meshweights = (float *) (mesh->pInterleaved + (long)mesh->sBoneWeight.pData + mesh->sBoneWeight.nStride * indices[v+a]);
+					unsigned char *meshbones = (unsigned char *) (mesh->pInterleaved + (long)mesh->sBoneIdx.pData + mesh->sBoneIdx.nStride * indices[v+a]);
+
+					
+					btVector3 temppos(0,0,0);
+					
+
+					uvs[a][0] = meshuvs[0];
+					uvs[a][1] = meshuvs[1];
+
+					for(unsigned int b = 0; b < mesh->sBoneIdx.n; b++)
+						bones[a][b] = meshbones[b];
+
+					for(unsigned int w = 0; w < mesh->sBoneWeight.n; w++)
+					{
+						float weight = meshweights[w];
+						unsigned char boneidx = meshbones[w];
+
+						if(weight < 0.00001f)
+							continue;
+
+						btVector3 p(meshverts[0], meshverts[1], meshverts[2]);
+						p = boneworldbt[boneidx] * p;
+						p = p * weight;
+						temppos += p;
+					}
+					
+					verts[a][0] = temppos[0];
+					verts[a][1] = temppos[1];
+					verts[a][2] = temppos[2];
+				}
+
+
+				glVertexPointer(3, GL_FLOAT, 0, verts);
+				glTexCoordPointer(2, GL_FLOAT, 0, uvs);
+		
+				//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+			}
+		}
+	
+	}
+
 #endif
 }
